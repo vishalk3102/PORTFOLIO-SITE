6c3baa8bbe20bdb6b63e384198d13673577e2bdb
vishalk3102 <vishal.k3102@gmail.com>
2022-08-21 17:15:33 +0530

    header framer motion code fixed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,7 +13,7 @@ const Header = (props) => {
       >
         <div className="w-[75%] h-[0.1rem]  bg-white"></div>
 
-        <span className="text-white uppercase font-bold text-center p-2 text-2xl">
+        <span className="text-[#fff] uppercase font-bold text-center p-2 text-2xl">
           {props.index === 1 ? (
             <motion.div
               initial={{ opacity: 0, x: "0px" }}
